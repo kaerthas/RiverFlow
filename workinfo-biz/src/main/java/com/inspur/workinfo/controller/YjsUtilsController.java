81232a90eec7417b13dc1816891dc0da01af8723
王翰鹏 <wanghanpeng@inspur.com>
2023-05-05 09:23:34 +0800

    自定义  公钥私钥 进行加解密

--- workinfo-biz/src/main/java/com/inspur/workinfo/controller/YjsUtilsController.java
+++ workinfo-biz/src/main/java/com/inspur/workinfo/controller/YjsUtilsController.java
@@ -13,15 +13,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/yjsUtils")
 @Api(value = "yjsUtils", tags = "一件事根据传参获取相关加密后的值")
 public class YjsUtilsController {
-    private static String publicKey = "04A5BFFA48FACB5F48E928A68046183AABFFEBB904A53CCEF2C834C73A639FE1FB43FBF54F89E94B16C0B6BD7EEB6C6E91123166005C91CEDFA92E12B1AC0F70A5";
-    private static String privateKey =  "00B0A2E24F56B672E291D3DA16283952C26FA2EB196318723E1838C3073CB0F364";
+//    private static String publicKey = "04A5BFFA48FACB5F48E928A68046183AABFFEBB904A53CCEF2C834C73A639FE1FB43FBF54F89E94B16C0B6BD7EEB6C6E91123166005C91CEDFA92E12B1AC0F70A5";
+//    private static String privateKey =  "00B0A2E24F56B672E291D3DA16283952C26FA2EB196318723E1838C3073CB0F364";
 
 
     @ApiOperation(value = "一件事根据传参获取相关加密后的值", notes = "一件事根据传参获取相关加密后的值")
     @PostMapping("/getSM2ForYjs" )
     public R getSM2ForYjs(@RequestBody JSONObject jso){
         try{
-           return R.ok().setData( SM2Utils.encrypt(publicKey, jso.toJSONString()));
+
+            String  publicKey  = jso.getString("publicKey");
+
+            JSONObject  params      = jso.getJSONObject("params");
+
+           return R.ok().setData( SM2Utils.encrypt(publicKey, params.toJSONString()));
         }catch (Exception e){
             e.printStackTrace();
             return R.failed().setMsg("查询失败！！");
@@ -31,7 +36,7 @@ public class YjsUtilsController {
 
     @ApiOperation(value = "一件事根据传参获取相关加密后的值", notes = "一件事根据传参获取相关加密后的值")
     @PostMapping("/decryptedSM2ForYjs" )
-    public R getSM2ForYjs(@RequestParam("jso") String  jso){
+    public R getSM2ForYjs(@RequestParam("jso") String  jso,@RequestParam("privateKey") String privateKey){
         try{
             return R.ok().setData( SM2Utils.decrypt(privateKey, jso));
         }catch (Exception e){
