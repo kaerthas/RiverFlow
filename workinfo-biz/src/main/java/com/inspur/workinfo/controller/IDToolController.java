43c9a8f7125b83ffbc380d213d9b14cf4d9237c3
harry <harry_ink@163.com>
2023-06-16 09:29:49 +0800

    级联查询地址再封装

--- workinfo-biz/src/main/java/com/inspur/workinfo/controller/IDToolController.java
+++ workinfo-biz/src/main/java/com/inspur/workinfo/controller/IDToolController.java
@@ -24,7 +24,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @AllArgsConstructor
@@ -35,14 +39,22 @@ public class IDToolController {
     private final RegionalInstitutionService regionalInstitutionService;
     private final RestTemplate restTemplate;
     private static final String appidcs = "200861_app_16703133694014546";
-    private static final String requrl004cs = "http://61.185.238.218:8043/svcreg/sxbus/api/application/api/sxzwfw/YJSYCB004_cs";
-    private static final String requrl005cs = "http://61.185.238.218:8043/svcreg/sxbus/api/application/api/sxzwfw/YJSYCB005_cs";
-//    private static final String requrl004cs = "http://59.218.251.18:33521/sxbus/api/application/api/sxzwfw/YJSYCB004_cs";
-//    private static final String requrl005cs = "http://59.218.251.18:33521/sxbus/api/application/api/sxzwfw/YJSYCB005_cs";
+//    private static final String requrl004cs = "http://61.185.238.218:8043/svcreg/sxbus/api/application/api/sxzwfw/YJSYCB004_cs";
+//    private static final String requrl005cs = "http://61.185.238.218:8043/svcreg/sxbus/api/application/api/sxzwfw/YJSYCB005_cs";
+    private static final String requrl004cs = "http://59.218.251.18:33521/sxbus/api/application/api/sxzwfw/YJSYCB004_cs";
+    private static final String requrl005cs = "http://59.218.251.18:33521/sxbus/api/application/api/sxzwfw/YJSYCB005_cs";
 
     private static final String appidzs = "200861_app_20201118153052";
+//    private static final String requrl004zs = "https://zwfwxtzx.shaanxi.gov.cn:8202/sxbus/api/application/api/sxzwfw/YJSYCB004";
+//    private static final String requrl005zs = "https://zwfwxtzx.shaanxi.gov.cn:8202/sxbus/api/application/api/sxzwfw/YJSYCB005";
     private static final String requrl004zs = "http://59.218.251.20:33507/sxbus/api/application/api/sxzwfw/YJSYCB004";
     private static final String requrl005zs = "http://59.218.251.20:33507/sxbus/api/application/api/sxzwfw/YJSYCB005";
+
+//    private static final String reqAddressUrlcs = "http://61.185.238.218:8043/svcreg/mbs/api/publicTransferInApplyRe/queryAddressForOnline";
+    private static final String reqAddressUrlcs = "http://59.218.251.18:33521/mbs/api/publicTransferInApplyRe/queryAddressForOnline";
+    private static final String reqAddressUrlzs = "http://59.218.251.20:33507/mbs/api/publicTransferInApplyRe/queryAddressForOnline";
+
+    private static final Map<String, JSONObject> jlData = new HashMap<>();
 
     @ApiOperation(value = "通过区域编码,下拉状态查询信息", notes = "通过区域编码,下拉状态查询信息")
     @PostMapping("/getRegInsInfo" )
@@ -114,6 +126,73 @@ public class IDToolController {
                 try{
                     return R.ok(this.getTXinfo(idCard, appidzs, requrl004zs, requrl005zs), "查询成功");
                 }catch (Exception e){
+                    e.printStackTrace();
+                    return R.failed(e.getMessage());
+                }
+            default:
+                return R.failed("请选择正确的环境");
+        }
+    }
+
+    @ApiOperation(value = "级联查询地址信息", notes = "级联查询地址信息")
+    @GetMapping("/queryAddressForOnline")
+    public R queryAddressForOnline(@RequestParam("env") String env){
+        switch (env){
+            case "test":
+                try{
+//                    JSONArray jsonArray = JSONArray.parseArray("[\n" +
+//                            "\t{\n" +
+//                            "\t\t\"id\":\"100\",\n" +
+//                            "\t\t\"parent\":\"#\",\n" +
+//                            "\t\t\"rootid\":\"#\",\n" +
+//                            "\t\t\"value\": \"110000\",\n" +
+//                            "\t\t\"label\": \"北京市\",\n" +
+//                            "\t},\n" +
+//                            "\t{\n" +
+//                            "\t\t\"id\":\"100001\",\n" +
+//                            "\t\t\"parent\":\"100\",\n" +
+//                            "\t\t\"rootid\":\"#\",\n" +
+//                            "\t\t\"value\": \"110104\",\n" +
+//                            "\t\t\"label\": \"宣武区\",\n" +
+//                            "\t},\n" +
+//                            "\t{\n" +
+//                            "\t\t\"id\":\"100001\",\n" +
+//                            "\t\t\"parent\":\"100\",\n" +
+//                            "\t\t\"rootid\":\"#\",\n" +
+//                            "\t\t\"value\": \"110104\",\n" +
+//                            "\t\t\"label\": \"新区测试\",\n" +
+//                            "\t},\n" +
+//                            "\t{\n" +
+//                            "\t\t\"id\":\"101\",\n" +
+//                            "\t\t\"parent\":\"#\",\n" +
+//                            "\t\t\"rootid\":\"#\",\n" +
+//                            "\t\t\"value\": \"610000\",\n" +
+//                            "\t\t\"label\": \"陕西省\",\n" +
+//                            "\t},\n" +
+//                            "\t{\n" +
+//                            "\t\t\"id\":\"101001\",\n" +
+//                            "\t\t\"parent\":\"101\",\n" +
+//                            "\t\t\"rootid\":\"#\",\n" +
+//                            "\t\t\"value\": \"610100\",\n" +
+//                            "\t\t\"label\": \"西安市\",\n" +
+//                            "\t},\n" +
+//                            "\t{\n" +
+//                            "\t\t\"id\":\"101001001\",\n" +
+//                            "\t\t\"parent\":\"101001\",\n" +
+//                            "\t\t\"rootid\":\"#\",\n" +
+//                            "\t\t\"value\": \"610102\",\n" +
+//                            "\t\t\"label\": \"新城区\",\n" +
+//                            "\t},\n" +
+//                            "]");
+//                    return R.ok(jsonArray, "查询成功");
+                    return R.ok(this.getAddressInfo(appidcs, reqAddressUrlcs), "查询成功");
+                }catch (Exception e){
+                    return R.failed(e.getMessage());
+                }
+            case "prov":
+                try{
+                    return R.ok(this.getAddressInfo(appidzs, reqAddressUrlzs), "查询成功");
+                }catch (Exception e){
                     return R.failed(e.getMessage());
                 }
             default:
@@ -158,7 +237,7 @@ public class IDToolController {
         String aac001 = listArray.getJSONObject(0).getJSONObject("ac01").get("aac001").toString();
         String aac002 = listArray.getJSONObject(0).getJSONObject("ac01").get("aac002").toString();
         String aac003 = listArray.getJSONObject(0).getJSONObject("ac01").get("aac003").toString();
-        String aab998 = listArray.getJSONObject(0).getJSONObject("ab01").get("aab998").toString();
+        String aab998 = listArray.getJSONObject(0).getJSONObject("ab01").get("aab998")==null ? "" : listArray.getJSONObject(0).getJSONObject("ab01").get("aab998").toString();
 
         requestDtoParam.clear();
         requestDtoParam.put("aac001", aac001);
@@ -170,8 +249,93 @@ public class IDToolController {
         HttpEntity<JSONObject> httpEntity500 = new HttpEntity<JSONObject>(reqBody, headers);
         //发送请求
         ResponseEntity<String> resp500Entity = restTemplate.postForEntity(url005, httpEntity500, String.class);
-        JSONObject jsonObject500 = JSONObject.parseObject(resp500Entity.getBody());
-        return jsonObject500;
+        JSONObject jsonObject005 = JSONObject.parseObject(resp500Entity.getBody());
+//        if(jsonObject500.getJSONObject("data")!=null && )
+//        JSONObject result005Values = jsonObject500.getJSONObject("data").getJSONObject("result");
+        return jsonObject005;
     }
 
+    private JSONArray getAddressInfo(String appid, String requrl) throws Exception{
+        //设置请求header信息
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type","application/json");
+        headers.add("C-Tenancy-Id","610000000000");
+        headers.add("C-App-Id", appid);
+
+        //封装请求头和内容
+        HttpEntity<JSONObject> httpEntity = new HttpEntity<JSONObject>(null, headers);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(requrl, httpEntity, String.class);
+        JSONObject jsonObject = JSONObject.parseObject(responseEntity.getBody());
+        JSONArray jsonArray = jsonObject.getJSONArray("data");
+        List<JSONObject> shanxi = jsonArray.stream().filter(e -> "陕西省".equals(((JSONObject) e).getString("label"))).map(e -> (JSONObject) e).collect(Collectors.toList());
+        List<JSONObject> provices = jsonArray.stream().filter(e -> ((JSONObject) e).getBoolean("disabled") == false && !"陕西省".equals(((JSONObject) e).getString("label"))).map(e -> (JSONObject) e).collect(Collectors.toList());
+        JSONArray resp = createJLData(shanxi);
+        resp.addAll(createJLData(provices));
+        return resp;
+    }
+
+    private JSONArray createJLData(List<JSONObject> param){
+        JSONArray resp = new JSONArray();
+        JSONArray jsonprovices = JSONArray.parseArray(param.toString());
+        for(int i=0;i<jsonprovices.size();i++){
+            JSONObject provice = jsonprovices.getJSONObject(i);
+            JSONObject jlEle = new JSONObject();
+            jlEle.put("value", provice.getString("value"));
+            jlEle.put("label", provice.getString("label"));
+            jlEle.put("rootid", "#");
+            jlEle.put("parent","#");
+            String proviceid = (100+i)+"";
+            jlEle.put("id", proviceid);
+            resp.add(jlEle);
+            JSONArray citys = provice.getJSONArray("children");
+            if(citys != null && citys.size()!=0){
+//                List<JSONObject> citysList = citys.stream().filter(e -> ((JSONObject) e).getBoolean("disabled") == false).map(e -> (JSONObject) e).collect(Collectors.toList());
+//                JSONArray cityJson = JSONArray.parseArray(citysList.toString());
+                for(int j=0; j<citys.size(); j++){
+                    jlEle = new JSONObject();
+                    JSONObject city = citys.getJSONObject(j);
+                    jlEle.put("value", city.getString("value"));
+                    jlEle.put("label", city.getString("label"));
+                    jlEle.put("rootid", "#");
+                    jlEle.put("parent", proviceid);
+                    String cityid = proviceid+"00"+(j+1);
+                    jlEle.put("id", cityid);
+                    resp.add(jlEle);
+                    JSONArray regions = city.getJSONArray("children");
+                    if(regions !=null && regions.size()!=0){
+//                        List<JSONObject> regionList = regions.stream().filter(e -> ((JSONObject) e).getBoolean("disabled") == false).map(e -> (JSONObject) e).collect(Collectors.toList());
+//                        JSONArray regionJson = JSONArray.parseArray(regionList.toString());
+                        for(int k=0; k<regions.size(); k++){
+                            jlEle = new JSONObject();
+                            JSONObject region = regions.getJSONObject(k);
+                            jlEle.put("value", region.getString("value"));
+                            jlEle.put("label", region.getString("label"));
+                            jlEle.put("rootid", "#");
+                            jlEle.put("parent", cityid);
+                            String regionid = cityid+"00"+(k+1);
+                            jlEle.put("id", regionid);
+                            resp.add(jlEle);
+                            JSONArray xies = region.getJSONArray("children");
+                            if(xies!=null && xies.size()!=0){
+//                                List<JSONObject> xieList = xies.stream().filter(e -> ((JSONObject) e).getBoolean("disabled") == false).map(e -> (JSONObject) e).collect(Collectors.toList());
+//                                JSONArray xieJson = JSONArray.parseArray(xieList.toString());
+                                for(int t=0;t<xies.size(); t++){
+                                    jlEle = new JSONObject();
+                                    JSONObject xie = xies.getJSONObject(t);
+                                    jlEle.put("value", xie.getString("value"));
+                                    jlEle.put("label", xie.getString("label"));
+                                    jlEle.put("rootid", "#");
+                                    jlEle.put("parent", regionid);
+                                    String xieid = regionid+"00"+(t+1);
+                                    jlEle.put("id", xieid);
+                                    resp.add(jlEle);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return resp;
+    }
 }
