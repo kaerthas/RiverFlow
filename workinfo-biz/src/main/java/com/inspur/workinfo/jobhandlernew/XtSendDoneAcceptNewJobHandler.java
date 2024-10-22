b32f96516447202d5d3d187b3e0da535406d1c85
15229315025 <wanghanpeng@inspur.com>
2024-10-22 09:42:55 +0800

    redis键值错误修改

--- workinfo-biz/src/main/java/com/inspur/workinfo/jobhandlernew/XtSendDoneAcceptNewJobHandler.java
+++ workinfo-biz/src/main/java/com/inspur/workinfo/jobhandlernew/XtSendDoneAcceptNewJobHandler.java
@@ -78,7 +78,7 @@ public class XtSendDoneAcceptNewJobHandler extends IJobHandler {
             }catch (Exception e){
                 e.printStackTrace();
             }finally {
-                redisCache.deleteObject(CommonConstants.XT_BUSINESS_SEND_DONE_NEW_REDIS);
+                redisCache.deleteObject(CommonConstants.XT_BUSINESS_SEND_ACCEPT_NEW_REDIS);
 
             }
         }else{
